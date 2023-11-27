93edbbca7ce8908874892aa6fd29363ae3610111
Андрей Боев <a.boev.it@gmail.com>
2023-11-27 23:50:13 +0300

    Рефакторинг кода

--- Шифр Виженера.py
+++ Шифр Виженера.py
@@ -1,32 +1,24 @@
-alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+def crypt(text: str, key: str, func):
+    alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    res = ''
+    key_index = 0
+    for c in text:
+        sym_pos = alphabet.find(c.lower())
+        key_pos = alphabet.find(key[key_index % len(key)].lower())
+        if sym_pos != -1:
+            res += func(sym_pos, key_pos, alphabet)
+            key_index += 1
+        else:
+            res += c
+    return res
 
 
 def encrypt(text: str, key: str) -> str:
-    res = ''
-    key_index = 0
-    for c in text:
-        sym_pos = alphabet.find(c.lower())
-        key_pos = alphabet.find(key[key_index % len(key)].lower())
-        if sym_pos != -1:
-            res += alphabet[(sym_pos + key_pos) % len(alphabet)]
-            key_index += 1
-        else:
-            res += c
-    return res
+    return crypt(text, key, lambda sym_pos, key_pos, alphabet: alphabet[(sym_pos + key_pos) % len(alphabet)])
 
 
 def decrypt(text: str, key: str) -> str:
-    res = ''
-    key_index = 0
-    for c in text:
-        sym_pos = alphabet.find(c.lower())
-        key_pos = alphabet.find(key[key_index % len(key)].lower())
-        if sym_pos != -1:
-            res += alphabet[(sym_pos - key_pos) % len(alphabet)]
-            key_index += 1
-        else:
-            res += c
-    return res
+    return crypt(text, key, lambda sym_pos, key_pos, alphabet: alphabet[(sym_pos - key_pos) % len(alphabet)])
 
 
 key = open('key.txt').read()
